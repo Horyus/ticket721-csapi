a9b087c0eb41637fa57b3b4068e989414c868536
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-06-13 17:38:43 +0200

    Initial Commit
    - Empty class
    - Babel setup
    
    T721CSA

---
+++ es6/index.js
@@ -0,0 +1,15 @@
+export class T721CSAPI {
+
+    constructor(url, password) {
+        this.url = url;
+        this.password = password;
+    }
+
+    register() {
+
+    }
+
+    connect() {
+
+    }
+}
